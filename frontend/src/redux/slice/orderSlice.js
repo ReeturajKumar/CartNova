1ccc8e26920f7d14012296b3e4f950824ba7c468
Reeturaj Kumar <reeturajvats587@gmail.com>
2025-04-25 01:13:08 +0530

    orderSlice

---
+++ frontend/src/redux/slice/orderSlice.js
@@ -0,0 +1,74 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+// fetcging all orders for user
+export const fetchUserOrders = createAsyncThunk("order/fetchUserOrders", async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/orders/my-orders`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
+
+// fetching details of order
+export const fetchOrderDetails = createAsyncThunk("order/fetchOrderDetails", async (orderId, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/orders/my-orders/${orderId}`, {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
+
+
+const orderSlice = createSlice({
+  name: "order",
+  initialState: {
+    orders: [],
+    totalOrders: 0,
+    orderDetails: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserOrders.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUserOrders.fulfilled, (state, action) => {
+        state.loading = false;
+        state.orders = action.payload;
+      })
+      .addCase(fetchUserOrders.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      })
+
+      .addCase(fetchOrderDetails.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchOrderDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.orderDetails = action.payload;
+      })
+      .addCase(fetchOrderDetails.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      })
+  },
+})
+
+export default orderSlice.reducer
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,13 +3,15 @@ import authReducer from "./slice/authSlice";
 import productsReducer from "./slice/productsSlice";
 import cartReducer from "./slice/cartSLice";
 import cheeckoutReducer from "./slice/checkOutSlice";
+import orderReducer from "./slice/orderSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products:productsReducer,
     cart:cartReducer,
-    checkout: cheeckoutReducer
+    checkout: cheeckoutReducer,
+    order: orderReducer
   },
 });
 
